3d43543ae3b731261c20c908899fd3dba0cd66fb
AM KK <andrzej.kapczynski@op.pl>
2021-02-24 00:31:38 +0100

    Adding project and its files

---
+++ stickman.py
@@ -0,0 +1,83 @@
+from kivy.properties import NumericProperty, BooleanProperty
+from kivy.uix.image import Image
+from kivy.clock import Clock, ClockEvent
+from kivy.core.window import Window
+
+
+class StickMan(Image):
+    velocity = NumericProperty(0)
+    lifes = BooleanProperty(True)
+    double_jump = NumericProperty(0)
+    frames = 0
+    sliding = NumericProperty(0)
+    game_active = BooleanProperty(False)
+
+    # start some action
+    def start_run(self):
+        self.frames = Clock.schedule_interval(self.run, 1/40.)
+
+    def start_slide(self):
+        self.sliding = 0
+        self.frames = Clock.schedule_interval(self.slide, 1/20.)
+
+    def start_jump(self):
+        self.frames = Clock.schedule_interval(self.jump, 1/16.)
+
+    # stop current action
+    def stop_current_action(self):
+        self.sliding = 0
+        if self.frames:
+            self.frames.cancel()
+
+    # action
+    def jump(self, time_passed):
+        frame = int(self.source[6:7])
+        if frame >= 7:
+            self.source = 'jumpi/7.png'
+        else:
+            frame += 1
+            self.source = 'jumpi/{}.png'.format(frame)
+
+    def run(self, time_passed):
+        frame = int(self.source[6:7])
+        if frame == 9:
+            frame = 1
+        else:
+            frame += 1
+        self.source = 'runin/{}.png'.format(frame)
+
+    def slide(self, time_passed):
+        frame = int(self.source[6:7])
+        if frame >= 5:
+            self.source = 'slide/5.png'
+        else:
+            frame += 1
+            self.source = 'slide/{}.png'.format(frame)
+
+    # events
+    def on_touch_down(self, touch):
+        if self.game_active:
+            if touch.pos[0] > Window.width/2:
+                self.stop_current_action()
+                self.start_jump()
+                self.double_jump += 1
+                if self.double_jump <= 2:
+                    self.velocity = Window.height * 0.32
+            else:
+                self.stop_current_action()
+                self.size = (Window.height/5, 118*(Window.height/5/191))
+                self.start_slide()
+
+        super().on_touch_down(touch)
+
+    def on_touch_up(self, touch):
+        if touch.pos[0] < Window.width / 2:
+            self.stop_current_action()
+            self.size = (182*(Window.height/5/211), Window.height/5)
+            self.start_run()
+        else:
+            self.size = (182 * (Window.height / 5 / 211), Window.height / 5)
+            self.stop_current_action()
+            self.start_run()
+
+        super().on_touch_up(touch)
---
+++ tut.py
@@ -0,0 +1,163 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ObjectProperty, BooleanProperty
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.clock import Clock
+from random import choice
+
+from pipe import Pipe
+from stickman import StickMan
+
+
+class Background(Widget):
+    floor_texture = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Create textures
+        self.floor_texture = Image(source="floor.png").texture
+        self.floor_texture.wrap = 'repeat'
+        self.floor_texture.uvsize = (Window.width / self.floor_texture.width, -1)
+
+    def scroll_textures(self, time_passed):
+        # update pos
+        self.floor_texture.uvpos = ((self.floor_texture.uvpos[0] + time_passed*4) % Window.width, self.floor_texture.uvpos[1])
+
+        # redraw textures
+        texture = self.property('floor_texture')
+        texture.dispatch(self)
+
+
+class TutApp(App):
+    pipes = []
+    GRAVITY = Window.height * 0.8
+    die_flag = 0
+    game_active = False
+    speed = 1
+    floor_height = 60
+
+    # stickman movement
+    def move_stickman(self, time_passed):
+        stickman = self.root.ids.stickman
+        stickman.y = stickman.y + stickman.velocity * time_passed * 5
+
+        if stickman.y <= self.floor_height - stickman.sliding:
+            stickman.y = self.floor_height - stickman.sliding
+            stickman.velocity = 0
+            stickman.double_jump = 0
+
+        if stickman.top > Window.height:
+            stickman.top = Window.height
+            stickman.velocity = 0
+        else:
+            stickman.velocity = stickman.velocity - self.GRAVITY * time_passed
+
+        self.check_collision()
+        if self.root.ids.score.text[0] != "Y":
+            self.root.ids.score.text = str(int(self.root.ids.score.text)+1)
+
+        if self.speed <= 1.9:
+            self.speed += 0.0002
+
+    # checking collisions
+    def check_collision(self):
+        stickman = self.root.ids.stickman
+        if stickman.x < -stickman.size[0]:
+            if stickman.lifes:
+                stickman.pos = (60, self.floor_height)
+                stickman.lifes = False
+                self.root.ids.lifes.text = "LIFES: 1"
+            else:
+                self.root.ids.lifes.text = "GAME OVER"
+                self.game_over()
+        if -stickman.size[0] < stickman.x < 60:
+            stickman.x += 0.5
+
+        for pipe in self.pipes:
+            if pipe.collide_widget(stickman):
+
+                # bottom pipe
+                if stickman.y < (pipe.pipe_center - pipe.GAP_SIZE/2.0):
+                    if stickman.x > pipe.x + 3*pipe.size[0]/4:
+                        stickman.x = pipe.x + 3*pipe.size[0]/4
+                        stickman.x += pipe.size[0]/4
+                    else:
+                        stickman.x = pipe.x - stickman.size[0]
+                # top pipe
+                if stickman.top > (pipe.pipe_center + pipe.GAP_SIZE/2.0):
+                    stickman.x = pipe.x - stickman.size[0]
+
+    # game is over
+    def game_over(self):
+        stickman = self.root.ids.stickman
+        self.game_active = False
+        stickman.game_active = False
+        stickman = self.root.ids.stickman
+        self.root.ids.start_button.disabled = False
+        self.root.ids.start_button.opacity = 1
+        self.speed = 1
+        self.root.ids.score.text = "YOUR SCORE: {}".format(self.root.ids.score.text)
+
+        for pipe in self.pipes:
+            self.root.remove_widget(pipe)
+        self.frames.cancel()
+        stickman.stop_current_action()
+
+        stickman.pos = (-2*stickman.size[1], self.floor_height)
+        stickman.lifes = 2
+        self.die_flag = 0
+
+    # what to do in next frame
+    def next_frame(self, time_passed):
+        self.move_stickman(time_passed)
+        self.move_pipes(time_passed)
+        self.root.ids.background.scroll_textures(time_passed)
+
+    # start the game
+    def start_game(self):
+        if not self.game_active:
+            stickman = self.root.ids.stickman
+            stickman.pos = (60, self.floor_height)
+
+            self.game_active = True
+            stickman.game_active = True
+            self.pipes = []
+
+            self.frames = Clock.schedule_interval(self.next_frame, 1/60.)
+            self.root.ids.score.text = str(0)
+            self.root.ids.lifes.text = "LIFES: 2"
+
+            num_pipes = 20
+            factor = min(self.speed, 1.4)
+            distance_between_pipes = Window.width / (num_pipes - 18) * 2 * factor
+
+            for i in range(num_pipes):
+                pipe = Pipe()
+                pipe.pipe_center = choice([((self.root.height - 100) / 4 - 60), ((self.root.height - 100) / 2 + 50), (self.root.height - 100) - 10])
+                pipe.size_hint = (None, None)
+                pipe.pos = (Window.width + i*distance_between_pipes, self.floor_height)
+                pipe.size = (64, self.root.height - self.floor_height)
+
+                self.pipes.append(pipe)
+                self.root.add_widget(pipe)
+
+    def move_pipes(self, time_passed):
+        for pipe in self.pipes:
+            pipe.x -= time_passed * 100 * 6 * self.speed
+
+        num_pipes = 20
+        factor = min(self.speed, 1.4)
+        distance_between_pipes = Window.width / (num_pipes - 18) * 2 * factor
+
+        pipe_xs = list(map(lambda pipe: pipe.x, self.pipes))
+        right_most_x = max(pipe_xs)
+
+        if right_most_x <= Window.width - distance_between_pipes:
+            most_left_pipe = self.pipes[pipe_xs.index(min(pipe_xs))]
+            most_left_pipe.x = Window.width
+
+
+if __name__ == '__main__':
+    TutApp().run()
